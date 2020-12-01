0216f68fd7755cac88197388236d8dd142cea147
Konstantin S <knstntn@users.noreply.github.com>
2020-12-01 12:36:35 +0000

    Update test_01.py

--- test_01.py
+++ test_01.py
@@ -1,11 +1,14 @@
 # https://adventofcode.com/2020/day/1
 
 def func(filename: str) -> num:
-  with open(filename,'r') as fh
-    lines = [int(line) for line in fh.readlines()]
-    print(lines)
-    
-                  
+  sum = 2020
+  with open(filename,'r') as fh:
+    nums = [int(line) for line in fh.readlines()]
+    res = set()
+    for num in nums:
+      if num in res:
+        return num * (sum - num)
+      res.add(sum - num)
   return 0
 
 def test_answer1():
