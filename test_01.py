8039d01aa9781d270f6d92709039b49338e934c8
Konstantin S <knstntn@users.noreply.github.com>
2020-12-01 12:30:38 +0000

    Update test_01.py

--- test_01.py
+++ test_01.py
@@ -4,6 +4,6 @@ def func(file_name: str) -> num:
   return 0
 
 def test_answer1():
-    assert func('inputs/01.1) ==  514579
+    assert func('inputs/01.1') ==  514579
     
 print(func('inputs/01.2'))
