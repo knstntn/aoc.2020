6e38ed3f4f986d3ed79b732cb161d80c1f641646
Konstantin S <knstntn@users.noreply.github.com>
2020-12-01 12:49:09 +0000

    Create test_02.py

---
+++ test_02.py
@@ -0,0 +1,18 @@
+# https://adventofcode.com/2020/day/1#part2
+
+def func(filename: str) -> int:
+  sum = 2020
+  with open(filename,'r') as fh:
+    nums = [int(line) for line in fh.readlines()]
+    res = set()
+    for num in nums:
+      if num in res:
+        return num * (sum - num)
+      res.add(sum - num)
+  return 0
+
+def test_answer1():
+  assert func('samples/01.1') ==  241861950
+    
+def test_answer2():
+  assert func('samples/01.2') == -1
