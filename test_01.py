92ae8271034d149218a20b269b3cc990f4c18fb6
Konstantin S <knstntn@users.noreply.github.com>
2020-12-01 12:29:11 +0000

    Create test_01.py

---
+++ test_01.py
@@ -0,0 +1,9 @@
+# https://adventofcode.com/2020/day/1
+
+def func(file_name: str) -> num:
+  return 0
+
+def test_answer1():
+    assert func('inputs/01.1) ==  514579
+    
+print(func('inputs/01.2'))
