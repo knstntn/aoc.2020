42f7d83c58e76384c215173e07f9278882db5697
Konstantin S <knstntn@users.noreply.github.com>
2020-12-01 13:02:14 +0000

    Update test_02.py

--- test_02.py
+++ test_02.py
@@ -1,18 +1,17 @@
 # https://adventofcode.com/2020/day/1#part2
 
-from typing import List
+from typing import List, Optional
 
-def func(filename: str) -> int:
+def func(filename: str) -> Optional[int]:
   sum = 2020
   with open(filename,'r') as fh:
     nums = [int(line) for line in fh.readlines()]
     for i, num in enumerate(nums):
       res = calc(sum - num, nums, i)
-      if res != 0:
+      if res is not None:
         return res * num
-    return 0
     
-def calc(sum: int, nums: List[int], skip: int) -> int:
+def calc(sum: int, nums: List[int], skip: int) -> Optional[int]:
   res = set()
   for i, num in enumerate(nums):
     if i == skip:
@@ -21,7 +20,6 @@ def calc(sum: int, nums: List[int], skip: int) -> int:
     if num in res:
       return num * (sum - num)
     res.add(sum - num)
-  return 0
 
 def test_answer1():
   assert func('samples/01.1') ==  241861950
