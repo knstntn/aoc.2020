ad0783577be20dd9f7506ccee0d29c1d335acabd
Konstantin S <knstntn@users.noreply.github.com>
2020-12-01 12:32:20 +0000

    Update test_01.py

--- test_01.py
+++ test_01.py
@@ -1,6 +1,11 @@
 # https://adventofcode.com/2020/day/1
 
-def func(file_name: str) -> num:
+def func(filename: str) -> num:
+  with open(filename,'r') as fh
+    lines = [int(line) for line in fh.readlines()]
+    print(lines)
+    
+                  
   return 0
 
 def test_answer1():
