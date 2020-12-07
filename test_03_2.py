c5149101ebeabad0fef87437678547d147803323
Konstantin S <knstntn@users.noreply.github.com>
2020-12-07 20:55:24 +0000

    Update test_03_2.py

--- test_03_2.py
+++ test_03_2.py
@@ -3,21 +3,17 @@
 slopes = [
   (1,1), (3,1), (5,1), (7,1), (1,2)
 ]
+
 def func(filename: str) -> int:
   with open(filename,'r') as fh:
-    map = [line.strip() for line in fh.readlines()]
-
-  res = 1
-  length = len(map[0])
-  height = len(map)
-  for slope in slopes:
-    count, top, left = (0, 0, 0)
-    while top < height:
-      if map[top][left%length] == '#':
-        count += 1
-      top, left = (top + slope[1], left + slope[0])
-    res *= count
-  return res
+    map = [x.strip() for x in fh.readlines()]
+    res = 1
+    for slope in slopes:
+      res *= sum([
+        1 for i, line in enumerate(map)
+        if i%slope[1] == 0 and line[slope[0]*i%len(line)] == '#'
+      ])
+    return res
 
 def test_answer1():
   assert func('samples/03.1') ==  336
