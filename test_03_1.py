e6686a9394a561e5b303956d8daa40063c34e534
Konstantin S <knstntn@users.noreply.github.com>
2020-12-07 19:22:58 +0000

    Update test_03_1.py

--- test_03_1.py
+++ test_03_1.py
@@ -3,11 +3,13 @@
 def func(filename: str) -> int:
   with open(filename,'r') as fh:
     map = [x for x in fh.readlines()]
+    length = len(map[0])
+    slope = (1,3)
     top, left, count = (0,0,0)
     while top < len(map):
-      if map[top][left] == '#':
+      if map[top][left%length] == '#':
         count += 1
-      top, left = (top + 1, (left + 3)%len(map[top]))
+      top, left = (top + slope[0], left + slope[1])
     return count
 
 def test_answer1():
