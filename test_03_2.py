a9c39a4a2e408e90bf614ad9708f7b0ddfe29f70
Konstantin S <knstntn@users.noreply.github.com>
2020-12-07 19:42:30 +0000

    Create test_03_2.py

---
+++ test_03_2.py
@@ -0,0 +1,26 @@
+# https://adventofcode.com/2020/day/3#part2
+
+slopes = [
+  (1,1), (3,1), (5,1), (7,1), (1,2)
+]
+def func(filename: str) -> int:
+  with open(filename,'r') as fh:
+    map = [line.strip() for line in fh.readlines()]
+
+  res = 1
+  length = len(map[0])
+  height = len(map)
+  for slope in slopes:
+    count, top, left = (0, 0, 0)
+    while height > down:
+      if map[top][left%length] == '#':
+        count += 1
+      top, left = (top + slope[1], left + slope[0])
+    res *= count
+  return res
+
+def test_answer1():
+  assert func('samples/03.1') ==  336
+    
+def test_answer2():
+  assert func('samples/03.2') == 580
